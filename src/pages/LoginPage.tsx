89e9683f7c887bf602467ea88519353414f0df23
raihan mahmud <raihanmahmudiut@gmail.com>
2024-06-10 18:34:17 +0600

    added css

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,18 +2,16 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useRoles } from '../context/useRoles';
 
+
 const LoginPage: React.FC = () => {
   const { login, users } = useRoles();
-  console.log("Log => | file: LoginPage.tsx:7 | users:", users)
   const [userId, setUserId] = useState<number>(1);
-  console.log("Log => | file: LoginPage.tsx:8 | userId:", userId)
   const navigate = useNavigate();
 
   const handleLogin = () => {
     login(userId);
 
-    const user = users[userId-1];
-    console.log("Log => | file: LoginPage.tsx:14 | user:", user)
+    const user = users[userId - 1];
     if (user.roles.includes('admin')) {
       navigate('/admin');
     } else if (user.roles.includes('user')) {
@@ -26,14 +24,14 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <div>
+    <div className="login-container">
       <h1>Login Page</h1>
       <label htmlFor="user-select">Select User Role:</label>
-          <select id="user-select" onChange={(e) => {
-              setUserId(Number(e.target.value))
-              console.log("Log => | file: LoginPage.tsx:8 | userId:", userId)
-              console.log("Log => | file: LoginPage.tsx:7 | users:", users)
-      }}>
+      <select
+        id="user-select"
+        onChange={(e) => setUserId(Number(e.target.value))}
+        value={userId}
+      >
         <option value={1}>Admin</option>
         <option value={2}>User</option>
         <option value={3}>Guest</option>
